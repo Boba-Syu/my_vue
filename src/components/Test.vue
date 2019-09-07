3926ef0694a5b027b67b9d290294480d9fc19bd6
Boba-Syu <bobasyu@163.com>
2019-09-07 15:56:23 +0800

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        data() {
+            return {
+                msg: '这是一个测试模块',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
